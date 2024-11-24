98bd2bc588b70562862f295af5ee2a18b7a16472
Minh Hieu <mhieusz.23.dkc@gmail.com>
2024-11-24 11:12:33 +0700

    update logic update profile account

--- lib/ui/screen/profile/widget/button_update_background_widget.dart
+++ lib/ui/screen/profile/widget/button_update_background_widget.dart
@@ -1,15 +1,22 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:foodioo/repositories/blocs/profile/profile_bloc.dart';
 import 'package:foodioo/ui/General/message_over_screen.dart';
 import 'package:foodioo/ui/General/spacing_vertical_widget.dart';
 import 'package:foodioo/ui/General/svg_gen_size_widget.dart';
+import 'package:image_picker/image_picker.dart';
 
 import '../../../../Core/Constants/constant_stataue.dart';
 import '../../../../Core/Theme/assets.gen.dart';
+import '../../../../repositories/blocs/profile/profile_event.dart';
+import '../../../../repositories/blocs/profile/profile_state.dart';
 import '../../../General/customize_button_widget.dart';
 
 class ButtonUpdateBackgroundWidget extends StatelessWidget {
-  const ButtonUpdateBackgroundWidget({super.key});
-
+  const ButtonUpdateBackgroundWidget({super.key, required this.bloc});
+  final ProfileBloc bloc;
   @override
   Widget build(BuildContext context) {
     final heightScreen = MediaQuery.sizeOf(context).height;
@@ -43,44 +50,112 @@ class ButtonUpdateBackgroundWidget extends StatelessWidget {
                       const SpacingVerticalWidget(
                         height: 35,
                       ),
-                      Center(
-                        child: Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(
-                                  AppConstant.radiusLarge),
-                              border: Border.all(
-                                  color: colorIcon,
-                                  width: 1,
-                                  strokeAlign: BorderSide.strokeAlignOutside),
-                            ),
-                            padding: const EdgeInsets.all(
-                                AppConstant.paddingComponent),
-                            height: sizeIcon,
-                            width: sizeIcon,
-                            child:
-                                Assets.icons.imagePicker.svg(color: colorIcon)),
-                      ),
-                      const Expanded(child: SizedBox()),
-                      Center(
-                        child: Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(
-                                  AppConstant.radiusLarge),
-                              border: Border.all(
-                                  color: colorIcon,
-                                  width: 1,
-                                  strokeAlign: BorderSide.strokeAlignOutside),
-                            ),
-                            padding: const EdgeInsets.all(
-                                AppConstant.paddingComponent),
-                            height: sizeIcon,
-                            width: sizeIcon,
-                            child: Assets.icons.camera.svg(color: colorIcon)),
+                      BlocBuilder<ProfileBloc, ProfileState>(
+                        bloc: bloc,
+                        builder: (context, state) {
+                          if (state.isUpdateSuccess) {
+                            Navigator.pop(context);
+                          }
+                          if (state.dynamicUpdateField.isNotEmpty) {
+                            return Center(
+                              child: Stack(
+                                children: [
+                                  Image.file(File(state.dynamicUpdateField)),
+                                  Align(
+                                      alignment: Alignment.topRight,
+                                      child: GestureDetector(
+                                          onTap: () {
+                                            bloc.add(
+                                                RemoveChangeBackgroundImage());
+                                          },
+                                          child: Container(
+                                            margin: const EdgeInsets.all(12),
+                                            decoration: const BoxDecoration(
+                                              shape: BoxShape.circle,
+                                              color: Color.fromARGB(
+                                                  64, 84, 80, 80),
+                                            ),
+                                            padding: const EdgeInsets.all(
+                                                AppConstant.paddingIcon),
+                                            child: const Icon(
+                                              Icons.close,
+                                              color: Colors.white,
+                                            ),
+                                          ))),
+                                ],
+                              ),
+                            );
+                          }
+                          return Column(
+                            children: [
+                              Center(
+                                child: GestureDetector(
+                                  onTap: () async {
+                                    final file = await ImagePicker()
+                                        .pickImage(source: ImageSource.gallery);
+                                    if (file != null) {
+                                      bloc.add(ChangeBackgroundImage(
+                                          backgroundImageUrl: file.path));
+                                    }
+                                  },
+                                  child: Container(
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(
+                                            AppConstant.radiusLarge),
+                                        border: Border.all(
+                                            color: colorIcon,
+                                            width: 1,
+                                            strokeAlign:
+                                                BorderSide.strokeAlignOutside),
+                                      ),
+                                      padding: const EdgeInsets.all(
+                                          AppConstant.paddingComponent),
+                                      height: sizeIcon,
+                                      width: sizeIcon,
+                                      child: Assets.icons.imagePicker
+                                          .svg(color: colorIcon)),
+                                ),
+                              ),
+                              SizedBox(
+                                height: sizeIcon / 2,
+                              ),
+                              Center(
+                                child: GestureDetector(
+                                  onTap: () async {
+                                    final file = await ImagePicker()
+                                        .pickImage(source: ImageSource.camera);
+                                    if (file != null) {
+                                      bloc.add(ChangeBackgroundImage(
+                                          backgroundImageUrl: file.path));
+                                    }
+                                  },
+                                  child: Container(
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(
+                                            AppConstant.radiusLarge),
+                                        border: Border.all(
+                                            color: colorIcon,
+                                            width: 1,
+                                            strokeAlign:
+                                                BorderSide.strokeAlignOutside),
+                                      ),
+                                      padding: const EdgeInsets.all(
+                                          AppConstant.paddingComponent),
+                                      height: sizeIcon,
+                                      width: sizeIcon,
+                                      child: Assets.icons.camera
+                                          .svg(color: colorIcon)),
+                                ),
+                              ),
+                            ],
+                          );
+                        },
                       ),
                       const Expanded(child: SizedBox()),
                       CustomizeButtonWidget(
                         onPressed: () {
-                          MessageToast.showToast(context);
+                          // MessageToast.showToast(context);
+                          bloc.add(PostChangeBackgroundImage());
                         },
                         title: "Cập nhật",
                         isEnable: true,
